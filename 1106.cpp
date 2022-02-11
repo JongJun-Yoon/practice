8902b305b48ce35e4f2c22ef45742272cde63ad2
JongJun-Yoon <korea.jongjun.yoon@gmail.com>
2022-02-11 22:28:22 +0900

    220211

--- 1106.cpp
+++ 1106.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
-#include <vector>
-#define MAX 20+1
-
+#include <algorithm>
+#define CITY 20
+#define COST 1000
 using namespace std;
+
 int C, N;
-int cost[MAX], output[MAX];
-int dp[MAX][1001];
+int cost[CITY], output[CITY];
+int dp[100*COST + 1];
+
+void print(){
+	for(int i=1; i<=C; i++){
+		cout << dp[i] << ' ';
+	}
+	cout << '\n';
+}
 
 void getInput(){
 	cin >> C >> N;
-	for(int i=1; i<=N; i++){
+	for(int i=0; i<N; i++){
 		cin >> cost[i] >> output[i];
 	}
 }
 
 void sol(){
-	for(int i=0; i<=N; i++){
-		for(int j=0; j<=1000; j++){
-			dp[i][j] = max(dp[i-1][j], dp[]);
+	for(int i=1; i<=100*COST; i++){
+		for(int j=0; j<N; j++){
+			if(i % cost[j] == 0){
+				dp[i] = max(dp[i], (i / cost[j]) * output[j]);
+			}
+			if(i-cost[j] > 0){
+				dp[i] = max(dp[i], dp[i-cost[j]] + output[j]);
+			}
+		}
+		if(dp[i] >= C){
+			cout << i << '\n';
+			return;
 		}
 	}
 }
--- 2294.cpp
+++ 2294.cpp
@@ -6,19 +6,30 @@
 using namespace std;
 
 int n, k;
-vector<int> coin(MAX, 0), dp(MAX, INF);
+vector<int> coin(MAX, 0), dp(INF, INF);
+
+void print(){
+	for(int i=1; i<=k; i++)	cout << dp[i] << ' ';
+	cout << '\n';
+}
 
 void getInput(){
 	cin >> n >> k;
 	for(int i=0; i<n; i++)	cin >> coin[i];
-	sort(coin.begin(), coin.begin() + n);
+	sort(coin.begin(), coin.begin()+n);
 }
 
 void sol(){
-	if(k < coin[0]){
-		cout << "-1\n";
-		return;
+	dp[0] = 0;
+	for(int i=0; i<n; i++){
+		int m = coin[i];
+		dp[m] = 1;
+		for(int j=m+1; j<=k; j++){
+			dp[j] = min(dp[j], dp[j-m] + 1);
+		}
 	}
+	if(dp[k] == INF)	cout << "-1\n";
+	else	cout << dp[k] << '\n';
 }
 
 int main(){
