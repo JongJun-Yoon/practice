bd0905de54d38ae08a9000eb719482413cc9ec1f
JongJun-Yoon <korea.jongjun.yoon@gmail.com>
2022-02-21 00:15:06 +0900

    220220

--- 22868.cpp
+++ 22868.cpp
@@ -1,49 +1,70 @@
 #include <iostream>
-#include <set>
 #include <queue>
+#include <vector>
+#include <cstring>
+#include <algorithm>
 #define MAX 10000+1
 using namespace std;
 
 int N, M;
+vector<int> node[MAX];
 int S, E;
-set<int> v[MAX];
 bool visit[MAX];
+int before[MAX];
+int ans = 0;
 
 void getInput(){
 	cin >> N >> M;
 	for(int i=0; i<M; i++){
-		int temp;
-		cin >> temp;
-		v[i].insert(temp);
-		v[temp].insert(i);
+		int a, b;
+		cin >> a >> b;
+		node[a].push_back(b);
+		node[b].push_back(a);
+	}
+	for(int i=1; i<=N; i++){
+		sort(node[i].begin(), node[i].end());
 	}
 	cin >> S >> E;
 }
 
-void bfs(){
-	int length = 0;
+void bfs(int from, int to){
 	queue<int> q;
-	q.push(S);
-	visit[S] = true;
-	while(q.empty()){
-		int qsize = q.size();
-		length++;
-		while(qsize--){
-			int node = q.front();
-			visit[node] = true; 
-			q.pop();
-			for(int i=0; i<v[node].size(); i++){
-				int adj = v[next][i];
-				if(!visit[adj])	q.push(adj);
+	q.push(from);	visit[from] = true;
+	while(!q.empty()){
+		int cnt = q.size();
+		while(cnt--){
+			int now = q.front();	q.pop();
+			cout << ans << ":" << now << '\n';
+			if(now == to){
+				return;
+			}
+			for(int i=0; i<node[now].size(); i++){
+				int next = node[now][i];
+				if(!visit[next] && next > 0){
+					q.push(next);
+					visit[next] = true;
+					before[next] = now;
+				}
 			}
 		}
+		ans++;
 	}
+}
 
-	cout << length << '\n';
+void sol(){
+	bfs(S, E);
+	memset(visit, false, MAX*sizeof(bool));
+	int node = E;
+	for(int i=0; i<ans; i++){
+		visit[node] = true;
+		node = before[node];
+	}
+	bfs(E, S);
+	cout << ans << '\n';
 }
 
 int main(){
 	getInput();
-	bfs();
+	sol();
 	return 0;
 }
